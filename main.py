66545ebfa9cfdb339c2d5358bd40ce9793515aad
Ana Pujana <anapujanajimenez@gmail.com>
2024-04-04 00:30:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ from inference_assistant import inference
 from utils import create_assistant_from_config_file, upload_to_openai, export_assistant
 from exportChat import export_chat
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.set_page_config(
     page_title="MVP",
     page_icon="🤖",
