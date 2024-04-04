6bf248a5b1608ad02a66d23e7bb9bc594cebd383
Ana Pujana <anapujanajimenez@gmail.com>
2024-04-04 10:00:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,11 +23,16 @@ st.set_page_config(
 )
 
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-local_css("style.css")
+st.markdown(
+    """
+    <style>
+    .stActionButton, #MainMenu, button[class^='styles_terminalButton'] {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.title("MVP🤖")
 
