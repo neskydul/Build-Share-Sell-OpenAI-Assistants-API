c07fcab6b4c7d3cc09f179bd6341bd9e2b77dd02
Ana Pujana <anapujanajimenez@gmail.com>
2024-04-04 08:24:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,16 +21,13 @@ st.set_page_config(
         #'About': "# This is a simple web app to build, share and sell OpenAI Assistants API\n\n"
     #}
 )
-st.markdown(
-    """
-    <style>
-    .stActionButton, #MainMenu, button[class^='styles_terminalButton'] {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+local_css("style.css")
 
 st.title("MVP🤖")
 
